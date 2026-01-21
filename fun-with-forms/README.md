00dc5e164211f660bcdc7367d58918ef457b8274
Kobi Hari <kobi2294@yahoo.com>
2026-01-21 10:24:02 +0200

    demo 2 -styling

--- fun-with-forms/README.md
+++ fun-with-forms/README.md
@@ -1,3 +1,8 @@
 ## Demo 0 - Field is now `FormField`
 
 ## Demo 1 - Field Styling
+    - using `provideSignalFormsConfig` in the config file
+    - The `NG_STATUS_CLASSES` constant provides the original class names of reactive forms
+    - We can define out own class names for additional or alternative class name logic
+    - Each class is defined using a function that takes the form control state as an argument and returns a boolean
+    - The function is expected to run in reactive context, so we can use signals inside the function body
--- fun-with-forms/src/app/app.config.ts
+++ fun-with-forms/src/app/app.config.ts
@@ -2,10 +2,20 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideSignalFormsConfig } from '@angular/forms/signals';
+import { NG_STATUS_CLASSES } from '@angular/forms/signals/compat';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes), 
+    provideSignalFormsConfig({
+      // classes: NG_STATUS_CLASSES, 
+      classes: {
+        'invalid': ctrl => ctrl.state().invalid(), 
+        'touched': ctrl => ctrl.state().touched(), 
+        'marked-invalid': ctrl => ctrl.state().invalid() && ctrl.state().touched()
+      }
+    })
   ]
 };
